983b075c6e13e7e20f70f25e7ff0966e17937979
Matthew Brown <matthew.e.brown.17@gmail.com>
2023-09-04 21:17:07 -0400

    Rename 'gloog' to 'gloog-core'
    Allows 'gloog' to be used for a larger crate that combines the smaller
    ones

--- src/bin/ex1/main.rs
+++ src/bin/ex1/main.rs
@@ -2,8 +2,8 @@ use std::mem::size_of;
 
 use bytemuck::cast_slice;
 use glfw::{self, Context, Key, WindowEvent, WindowMode};
-use gloog as gl;
-use gloog::types::{ClearMask, BufferTarget, BufferUsage, DrawMode, Program, ShaderType, VertexAttribType};
+use gloog_core as gl;
+use gloog_core::types::{BufferTarget, BufferUsage, ClearMask, DrawMode, Program, ShaderType, VertexAttribType};
 use gloog_math::Vec3;
 
 
