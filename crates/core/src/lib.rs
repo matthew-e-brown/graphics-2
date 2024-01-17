5908988938783823c686a2925a21215455e7e322
Matthew Brown <matthew.e.brown.17@gmail.com>
2024-01-17 16:08:52 -0500

    Remove bindings-related crates
    They have been pulled into their own repository ('gloog').

--- crates/core/src/lib.rs
+++
@@ -1,4 +0,0 @@
-//! Gloog's bindings for OpenGL 4.6.
-
-// See build script and `gloog-bindgen` for details.
-include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
--- crates/gloog/src/lib.rs
+++
@@ -1,8 +0,0 @@
-//! Currently, this crate is a wrapper for two other "base" crates:
-//!
-//! - [`gloog-core`][gloog_core] &ndash; core OpenGL wrapper functions.
-//! - [`gloog-math`][gloog_math] &ndash; Mathematic data structures and functions.
-//!
-//! See those crates' documentation for details.
-
-pub use {gloog_core as core, gloog_math as math};
