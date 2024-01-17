da640bc0a8b1bb0ea7ebad3102af298156e18b6c
Matthew Brown <matthew.e.brown.17@gmail.com>
2024-01-17 15:06:51 -0500

    Move 'math' crate back into library module

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+/// Implementations of vectors, matrices, and other related types.
+///
+/// The structures defined by this module are primarily based off of
+/// - Khronos's wiki page on data types.
+/// - Foundations of Game Development, vol. 1 and 2 by Eric Lengyel.
+/// They are designed to work specifically with OpenGL for the purposes of this course (as opposed to "generic" vectors
+/// and matrices like one would find in something like [nalgebra]).
+///
+/// [nalgebra]: https://nalgebra.org/
+pub mod math;
