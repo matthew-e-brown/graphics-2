965a6fa00b01ce3132fe9e9fc48f734a82e798e4
Matthew Brown <matthew.e.brown.17@gmail.com>
2024-01-28 19:55:18 -0500

    Implement GLContext struct generation

--- crates/gl/build/gen.rs
+++ crates/gl/build/gen.rs
@@ -20,59 +20,120 @@ impl Generator for StructGenerator {
         write_custom_types(&enums, dest)?;
         writeln!(dest, "}}\n")?;
 
+        writeln!(dest, "#[allow(unused_imports)]")?;
         writeln!(dest, "use self::types::{{GLEnum, GLBitfield}};\n")?;
         write_enum_values(&[enums, bitfields, special_enums], dest)?;
+        writeln!(dest)?;
+
+        write_gl_struct(registry, dest)?;
 
         Ok(())
     }
 }
 
 
-fn get_typename(gl_type: &str) -> &'static str {
-    match gl_type {
+/// Converts a typename from how it appears in the raw OpenGL spec into one applicable for usage by this crate.
+fn gl_type_to_rs(gl_typename: &str) -> Option<&'static str> {
+    // cspell:disable
+    #[rustfmt::skip]
+    return match gl_typename {
         // Common types from OpenGL 1.1
-        "GLenum" => "GLEnum",                          // super::__gl_imports::raw::c_uint;
-        "GLboolean" => "bool",                         // super::__gl_imports::raw::c_uchar;
-        "GLbitfield" => "GLBitfield",                  // super::__gl_imports::raw::c_uint;
-        "GLvoid" => "core::ffi::c_void",               // super::__gl_imports::raw::c_void;
-        "GLbyte" => "i8",                              // super::__gl_imports::raw::c_char;
-        "GLshort" => "i16",                            // super::__gl_imports::raw::c_short;
-        "GLint" => "i32",                              // super::__gl_imports::raw::c_int;
-        "GLclampx" => "i32",                           // super::__gl_imports::raw::c_int;
-        "GLubyte" => "u8",                             // super::__gl_imports::raw::c_uchar;
-        "GLushort" => "u16",                           // super::__gl_imports::raw::c_ushort;
-        "GLuint" => "u32",                             // super::__gl_imports::raw::c_uint;
-        "GLsizei" => "i32",                            // super::__gl_imports::raw::c_int;
-        "GLfloat" => "f32",                            // super::__gl_imports::raw::c_float;
-        "GLclampf" => "f32",                           // super::__gl_imports::raw::c_float;
-        "GLdouble" => "f64",                           // super::__gl_imports::raw::c_double;
-        "GLclampd" => "f64",                           // super::__gl_imports::raw::c_double;
-        "GLeglImageOES" => "*const core::ffi::c_void", // *const super::__gl_imports::raw::c_void;
-        "GLchar" => "i8",                              // super::__gl_imports::raw::c_char;
-        "GLcharARB" => "i8",                           // super::__gl_imports::raw::c_char;
+        "GLenum"            => Some("GLEnum"),                      // super::__gl_imports::raw::c_uint;
+        "GLboolean"         => Some("bool"),                        // super::__gl_imports::raw::c_uchar;
+        "GLbitfield"        => Some("GLBitfield"),                  // super::__gl_imports::raw::c_uint;
+        "GLvoid"            => Some("core::ffi::c_void"),           // super::__gl_imports::raw::c_void;
+        "GLbyte"            => Some("i8"),                          // super::__gl_imports::raw::c_char;
+        "GLshort"           => Some("i16"),                         // super::__gl_imports::raw::c_short;
+        "GLint"             => Some("i32"),                         // super::__gl_imports::raw::c_int;
+        "GLclampx"          => Some("i32"),                         // super::__gl_imports::raw::c_int;
+        "GLubyte"           => Some("u8"),                          // super::__gl_imports::raw::c_uchar;
+        "GLushort"          => Some("u16"),                         // super::__gl_imports::raw::c_ushort;
+        "GLuint"            => Some("u32"),                         // super::__gl_imports::raw::c_uint;
+        "GLsizei"           => Some("i32"),                         // super::__gl_imports::raw::c_int;
+        "GLfloat"           => Some("f32"),                         // super::__gl_imports::raw::c_float;
+        "GLclampf"          => Some("f32"),                         // super::__gl_imports::raw::c_float;
+        "GLdouble"          => Some("f64"),                         // super::__gl_imports::raw::c_double;
+        "GLclampd"          => Some("f64"),                         // super::__gl_imports::raw::c_double;
+        "GLeglImageOES"     => Some("*const core::ffi::c_void"),    // *const super::__gl_imports::raw::c_void;
+        "GLchar"            => Some("i8"),                          // super::__gl_imports::raw::c_char;
+        "GLcharARB"         => Some("i8"),                          // super::__gl_imports::raw::c_char;
         // -----------------------------------------------------------------------------------------
-        #[cfg(target_os = "macos")]
-        "GLhandleARB" => "*const core::ffi::c_void", // *const super::__gl_imports::raw::c_void;
-        #[cfg(not(target_os = "macos"))]
-        "GLhandleARB" => "u32", // super::__gl_imports::raw::c_uint;
-        "GLhalfARB" => "u16",        // super::__gl_imports::raw::c_ushort;
-        "GLhalf" => "u16",           // super::__gl_imports::raw::c_ushort;
-        "GLfixed" => "i32",          // GLint; (Must be 32 bits)
-        "GLintptr" => "isize",       // isize;
-        "GLsizeiptr" => "isize",     // isize;
-        "GLint64" => "i64",          // i64;
-        "GLuint64" => "u64",         // u64;
-        "GLintptrARB" => "isize",    // isize;
-        "GLsizeiptrARB" => "isize",  // isize;
-        "GLint64EXT" => "i64",       // i64;
-        "GLuint64EXT" => "u64",      // u64;
-        "GLsync" => "*const GLSync", // *const __GLsync; (with `pub enum GLSync {}` above it)
+        #[cfg(target_os = "macos")]      "GLhandleARB" => Some("*const core::ffi::c_void"), // *const super::__gl_imports::raw::c_void;
+        #[cfg(not(target_os = "macos"))] "GLhandleARB" => Some("u32"),                      // super::__gl_imports::raw::c_uint;
+        "GLhalfARB"         => Some("u16"),                         // super::__gl_imports::raw::c_ushort;
+        "GLhalf"            => Some("u16"),                         // super::__gl_imports::raw::c_ushort;
+        "GLfixed"           => Some("i32"),                         // GLint; (Must be 32 bits)
+        "GLintptr"          => Some("isize"),                       // isize;
+        "GLsizeiptr"        => Some("isize"),                       // isize;
+        "GLint64"           => Some("i64"),                         // i64;
+        "GLuint64"          => Some("u64"),                         // u64;
+        "GLintptrARB"       => Some("isize"),                       // isize;
+        "GLsizeiptrARB"     => Some("isize"),                       // isize;
+        "GLint64EXT"        => Some("i64"),                         // i64;
+        "GLuint64EXT"       => Some("u64"),                         // u64;
+        "GLsync"            => Some("*const types::GLSync"),        // *const __GLsync; (with `pub enum GLSync {}` above it)
         // Vendor extension types
-        "GLhalfNV" => "u16",           // super::__gl_imports::raw::c_ushort;
-        "GLvdpauSurfaceNV" => "isize", // GLintptr;
+        "GLhalfNV"          => Some("u16"),                         // super::__gl_imports::raw::c_ushort;
+        "GLvdpauSurfaceNV"  => Some("isize"),                       // GLintptr;
         // -----------------------------------------------------------------------------------------
-        _ => panic!("unknown GLtype: {gl_type}"),
+        "GLDEBUGPROC"       => Some("types::GLDebugProc"),
+        "GLDEBUGPROCARB"    => Some("types::GLDebugProc"),
+        "GLDEBUGPROCKHR"    => Some("types::GLDebugProc"),
+        "GLDEBUGPROCAMD"    => Some("types::GLDebugProc_AMD"),
+        _ => None,
+    };
+    // cspell:enable
+}
+
+
+/// Converts a typename from how it appears after being parsed by [`gl_generator`] into one for usage by this crate.
+fn lib_type_to_rs(lib_typename: &str) -> Cow<'_, str> {
+    if lib_typename == "()" {
+        return lib_typename.into();
     }
+
+    let mut res = String::new();
+    let mut str = lib_typename;
+
+    /// Trims the given pattern off the start of the given slice, shrinking it down in the process.
+    ///
+    /// Returns the matched pattern for convenience.
+    fn trim_start_mut<'a, 'p>(str: &mut &'a str, pat: &'p str) -> Option<&'p str> {
+        if str.starts_with(pat) {
+            *str = &str[pat.len()..];
+            Some(pat)
+        } else {
+            None
+        }
+    }
+
+    // Trim off any pointer types and add to our own string
+    loop {
+        let Some(ptr) = trim_start_mut(&mut str, "*const").or_else(|| trim_start_mut(&mut str, "*mut")) else {
+            // Didn't start with either one; done.
+            break;
+        };
+
+        str = str.trim_start(); // Remove extra space after *const/*mut
+        res.push_str(ptr);
+        res.push(' ');
+    }
+
+    // Map aliases to our types
+    if let Some(_) = trim_start_mut(&mut str, "types::") {
+        let our_ty = gl_type_to_rs(str).unwrap_or_else(|| panic!("unknown typename: {lib_typename}"));
+        res.push_str(our_ty);
+    } else if let Some(_) = trim_start_mut(&mut str, "__gl_imports::") {
+        if let Some(_) = trim_start_mut(&mut str, "raw::c_void") {
+            res.push_str("core::ffi::c_void");
+        } else {
+            unimplemented!("unknown typename: {lib_typename}");
+        }
+    } else {
+        unimplemented!("unknown typename: {lib_typename}");
+    }
+
+    res.into()
 }
 
 
@@ -135,6 +196,7 @@ fn separate_enums(registry: &Registry) -> [BTreeSet<&Enum>; 3] {
 
 
 fn write_type_aliases(dest: &mut impl Write) -> io::Result<()> {
+    #[rustfmt::skip]
     dest.write_all(indoc! {r#"
         // Function pointers, used for callbacks.
         pub type GLDebugProc = Option<extern "system" fn(
@@ -226,10 +288,10 @@ fn write_custom_types(enums: &BTreeSet<&Enum>, dest: &mut impl Write) -> io::Res
         }
 
         // !
-        impl core::ops::Not for GLBitfield { type Output = Self; fn not(self) -> Self::Output { Self(!self.0) } }
+        impl core::ops::Not for GLBitfield { type Output = Self; fn not(self) -> Self::Output { GLBitfield(!self.0) } }
 
-        impl core::ops::Not for &'_ GLBitfield { type Output = GLBitfield; fn not(self) -> Self::Output { !*self } }
-        impl core::ops::Not for &'_ mut GLBitfield { type Output = GLBitfield; fn not(self) -> Self::Output { !*self } }
+        impl core::ops::Not for &'_ GLBitfield { type Output = GLBitfield; fn not(self) -> Self::Output { GLBitfield(!self.0) } }
+        impl core::ops::Not for &'_ mut GLBitfield { type Output = GLBitfield; fn not(self) -> Self::Output { GLBitfield(!self.0) } }
 
         // |, &, ^
         impl core::ops::BitOr for GLBitfield { type Output = Self; fn bitor(self, rhs: Self) -> Self::Output { GLBitfield(self.0 | rhs.0) } }
@@ -282,7 +344,7 @@ fn write_custom_types(enums: &BTreeSet<&Enum>, dest: &mut impl Write) -> io::Res
     for (val, idents) in enums_by_val {
         assert!(idents.len() > 0);
 
-        dest.write_all("            ".as_bytes())?; // indentation
+        write!(dest, "            ")?; // indentation
         match idents.len() {
             1 => dest.write_fmt(format_args!("0x{val:04X} => \"{}\".into(),\n", idents[0]))?,
             _ => dest.write_fmt(format_args!("0x{val:04X} => \"OneOf({})\".into(),\n", idents.join(", ")))?,
@@ -329,9 +391,60 @@ fn write_enum_values(enum_sets: &[BTreeSet<&Enum>; 3], dest: &mut impl Write) ->
     for &e in special_enums.iter().filter(|e| e.ty != "GLboolean") {
         let ident = convert_ident(&e.ident, Case::Snake, Case::UpperSnake);
         let value = &e.value;
-        let ty = get_typename(&e.ty);
+        let ty = gl_type_to_rs(&e.ty).unwrap_or_else(|| panic!("enum type {} should map to Rust type", e.ty));
         dest.write_fmt(format_args!("pub const {ident}: {ty} = {value};\n"))?;
     }
 
+    Ok(())
+}
+
+
+fn make_fn_ptr<'a, S: AsRef<str>>(params: impl IntoIterator<Item = (S, S)>, ret_ty: &str) -> String {
+    let mut result = String::from("extern \"system\" fn(");
+
+    let params = params
+        .into_iter()
+        .map(|(ident, ty)| format!("{}: {}", ident.as_ref(), ty.as_ref()))
+        .collect::<Vec<_>>()
+        .join(", ");
+
+    result += &params;
+    result += ")";
+
+    if ret_ty != "()" {
+        result += " -> ";
+        result += ret_ty;
+    }
+
+    result
+}
+
+
+fn write_gl_struct(registry: &Registry, dest: &mut impl Write) -> io::Result<()> {
+    // Create the OpenGL context struct
+    #[rustfmt::skip]
+    dest.write_all(indoc! {r#"
+        /// An abstraction over an OpenGL context.
+        ///
+        /// This struct _not really_ an "OpenGL context;" really, it is a collection of loaded function pointers for use
+        /// in the current thread.
+        pub struct GLContext {
+    "#}.as_bytes())?;
+
+    // println!("{:#?}", registry.cmds);
+    for cmd in &registry.cmds {
+        let ident = convert_ident(&cmd.proto.ident, Case::Camel, Case::Snake);
+        let ret_ty = lib_type_to_rs(&cmd.proto.ty);
+        let params = cmd.params.iter().map(|param| {
+            let ident = convert_ident(&param.ident, Case::UpperCamel, Case::Snake);
+            let ty = lib_type_to_rs(&param.ty);
+            (ident, ty)
+        });
+
+        let fn_ptr = make_fn_ptr(params, &ret_ty);
+        writeln!(dest, "    {ident}: Option<{fn_ptr}>,")?;
+    }
+
+    writeln!(dest, "}}")?;
     Ok(())
 }
